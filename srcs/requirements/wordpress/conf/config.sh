5b50830e1071816eee40c5be9aa59c50e965ce8d
mdev9 <devriesmarijn05@gmail.com>
2024-07-11 17:58:10 +0000

    finallyyyyyyyyyyyy

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-sleep 10;
+while ! mariadb -h mariadb -u$SQL_USER -p$SQL_PASSWORD <<< "SHOW databases;" &>/dev/null; do
+	echo "waiting for mariadb"
+	sleep 1
+done
 
 echo "Starting config.sh script..."
 
 # checks if wp-config.php exists, only executes the first time
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     echo "wp-config.php not found, creating..."
-    ls -la /var/www/html
+
+    cd /var/www/html
 
     wp config create --allow-root \
                      --dbname="${SQL_DATABASE}" \
@@ -18,7 +22,7 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 
     wp core install --allow-root \
                     --url=${WP_URL} \
-                    --title=${WP_TITLE} \
+                    --title="${WP_TITLE}" \
                     --admin_user=${WP_ADMIN} \
                     --admin_password=${WP_ADMIN_PASSWORD} \
                     --admin_email=${WP_ADMIN_EMAIL} \
@@ -33,3 +37,5 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 else
     echo "wp-config.php found."
 fi
+
+$@
